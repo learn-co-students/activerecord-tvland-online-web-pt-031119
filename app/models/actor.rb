fc41c51a72dfadcb1259a03a5d9fb6deefe0f39d
github username <github email address>
2019-06-04 22:46:35 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
+require 'Pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    p "#{self.first_name} #{self.last_name}"
+  end
+
+  def self.list_roles
+    self.shows
+    binding.pry
+  end
+  binding.pry
 end
