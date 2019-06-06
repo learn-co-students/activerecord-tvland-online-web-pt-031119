3d8fa983be8f0fb2fa7d03db3a689c781153abda
github username <github email address>
2019-06-05 20:53:02 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,13 +1,14 @@
+require 'Pry'
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
   def full_name
     p "#{self.first_name} #{self.last_name}"
-  end
+  end 
 
-  def self.list_roles
-    # self.shows
-
+  def list_roles
+    roles = self.characters.map {|c| c.name} + self.shows.map {|s| s.name}
+    roles.join(" - ")
   end
 end
