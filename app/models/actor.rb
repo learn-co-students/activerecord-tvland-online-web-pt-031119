4c138939266b1650d3affcafefc5b1698323c3e9
Samuel T <ssamt1142@gmail.com>
2019-06-13 03:32:47 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    name = [self.first_name, self.last_name]
+    full = name.join(" ")
+  end
+
+  def list_roles
+    char = []
+    show = []
+    self.characters.collect do |x|
+      char << x.name
+    end
+    self.shows.collect do |x|
+      show << x.name
+    end
+    list = [char, show]
+    lroles = list.flatten.join(" - ")
+  end
 end
